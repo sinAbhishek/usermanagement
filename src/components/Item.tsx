f0465fe756e81cfa9e7596bc03c32c61b2aeee5d
sinAbhishek <singhabhshek979@gmail.com>
2024-09-06 11:28:45 +0530

    final

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { User } from "../types/types";
 import { useNavigate } from "react-router-dom";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 interface UserItemProps {
   user: User;
   onEdit: (user: User) => void;
@@ -18,9 +20,11 @@ const Item: React.FC<UserItemProps> = ({ user, onEdit, onDelete }) => {
       <td className=" py-4 px-2">{user.email}</td>
       <td className=" py-4 px-2">{user.phone}</td>
       <td className=" py-4 px-2">
-        <button onClick={() => onEdit(user)}>Edit</button>
+        <button onClick={() => onEdit(user)}>
+          <MdEdit size={"1.2rem"} />
+        </button>
         <button className=" mx-2 w-max" onClick={() => onDelete(user.id)}>
-          Delete
+          <MdDelete color="red" size={"1.2rem"} />
         </button>
         <button
           className="view w-max px-4 py-1 rounded-full bg-green-400 text-sm text-white "
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,12 +4,13 @@ import { User } from "../types/types";
 import UserItem from "./Item";
 import EditModal from "./EditModal";
 import CreateModal from "./CreateModal";
-
+import { Blocks } from "react-loader-spinner";
 const List: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [editingUser, setEditingUser] = useState<User | null>(null);
   const [openEdit, setOpen] = React.useState(false);
   const [openCreate, setCreateOpen] = React.useState(false);
+  const [loading, setloading] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const handleCreateOpen = () => setCreateOpen(true);
@@ -20,7 +21,9 @@ const List: React.FC = () => {
 
   const loadUsers = async () => {
     try {
+      setloading(true);
       const usersData = await fetchUsers();
+      setloading(false);
       console.error(usersData);
       setUsers(usersData);
     } catch (error) {
@@ -65,45 +68,61 @@ const List: React.FC = () => {
   };
 
   return (
-    <div className="w-[90%] max-[768px]:w-full bg-[#f8f6f6] px-8 ">
-      <table className=" w-full ">
-        <thead className="w-full border-b border-slate-500">
-          <tr className="  w-full ">
-            <th className=" text-start py-2 px-2 ">Name</th>
-            <th className=" text-start py-2 px-2">Email</th>
-            <th className=" text-start py-2 px-2">Phone</th>
-            <th className=" text-start py-2 px-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <UserItem
-              key={user.id}
-              user={user}
-              onEdit={() => openEditor(user)}
-              onDelete={handleDelete}
-            />
-          ))}
-        </tbody>
-      </table>
-      <EditModal
-        data={editingUser}
-        handleClose={handleClose}
-        onSubmit={handleUpdate}
-        open={openEdit}
-      />
-      <CreateModal
-        onSubmit={handleCreate}
-        handleClose={handleCreateClose}
-        open={openCreate}
-      />
-      <button
-        onClick={() => handleCreateOpen()}
-        className="  fixed top-1 right-2 w-max px-4 py-2 rounded-md bg-orange-700 text-white font-medium"
-      >
-        Create
-      </button>
-    </div>
+    <>
+      {loading ? (
+        <div className="w-screen h-screen flex justify-center items-center">
+          <Blocks
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div className="w-[90%] max-[768px]:w-full bg-[#f8f6f6] px-8 ">
+          <table className=" w-full ">
+            <thead className="w-full border-b border-slate-500">
+              <tr className="  w-full ">
+                <th className=" text-start py-2 px-2 ">Name</th>
+                <th className=" text-start py-2 px-2">Email</th>
+                <th className=" text-start py-2 px-2">Phone</th>
+                <th className=" text-start py-2 px-2">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <UserItem
+                  key={user.id}
+                  user={user}
+                  onEdit={() => openEditor(user)}
+                  onDelete={handleDelete}
+                />
+              ))}
+            </tbody>
+          </table>
+          <EditModal
+            data={editingUser}
+            handleClose={handleClose}
+            onSubmit={handleUpdate}
+            open={openEdit}
+          />
+          <CreateModal
+            onSubmit={handleCreate}
+            handleClose={handleCreateClose}
+            open={openCreate}
+          />
+          <button
+            onClick={() => handleCreateOpen()}
+            className="  fixed top-1 right-2 w-max px-4 py-2 rounded-md bg-orange-700 text-white font-medium"
+          >
+            Create
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
